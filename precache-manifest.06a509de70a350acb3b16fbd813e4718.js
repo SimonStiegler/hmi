1e354788cb3f8d2fffd16df98f7cdb6db855de4d
Simon Stiegler <Simon.Stiegler@gmx.net>
2019-11-21 00:09:31 +0100

    Updates

---
+++ precache-manifest.06a509de70a350acb3b16fbd813e4718.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c17550359f7a83a30d206de3fd6ddde",
+    "url": "/hmi/index.html"
+  },
+  {
+    "revision": "0c3d2af4bcd722bb0134",
+    "url": "/hmi/static/css/2.e30031ea.chunk.css"
+  },
+  {
+    "revision": "552c0a13ebec949c0da3",
+    "url": "/hmi/static/css/main.e89723a8.chunk.css"
+  },
+  {
+    "revision": "0c3d2af4bcd722bb0134",
+    "url": "/hmi/static/js/2.fd08e7c5.chunk.js"
+  },
+  {
+    "revision": "552c0a13ebec949c0da3",
+    "url": "/hmi/static/js/main.0a4a823e.chunk.js"
+  },
+  {
+    "revision": "908d87767c8e29592ed6",
+    "url": "/hmi/static/js/runtime-main.5461d00d.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/hmi/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/hmi/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/hmi/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/hmi/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/hmi/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/hmi/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/hmi/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/hmi/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/hmi/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/hmi/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/hmi/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/hmi/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/hmi/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/hmi/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/hmi/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/hmi/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
